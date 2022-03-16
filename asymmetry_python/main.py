9441be2d8000f0b83e7f6d02bcac3edbbd1fa192
alessandrofelder <a.felder@ucl.ac.uk>
2022-03-16 12:05:49 +0000

    added functionality to plot histograms vertically in one plot

--- asymmetry_python/main.py
+++ asymmetry_python/main.py
@@ -5,15 +5,19 @@ import numpy as np
 folder_path = "C:\\Users\\henry\\OneDrive - University College London\\Project Work\\Image Analysis\\Images\\Cell shaver\\pixel_distance_python\\"
 
 
-for file_path in listdir(folder_path):
+for file_number, file_path in enumerate(listdir(folder_path)):
     input_image = read_image(folder_path+file_path)
     mask = image_mask(input_image)
     values_of_interest = apply_mask(input_image, mask)
-    plt.hist(values_of_interest)
+
+    fig, axs = plt.subplots(len(listdir(folder_path)))
+    fig.suptitle('Histograms')
+    axs[file_number].hist(values_of_interest)
     # plt.scatter(np.ones_like(values_of_interest), values_of_interest)
     #plt.boxplot(values_of_interest)
-    plt.show()
+
     mean, st_dev = mean_and_stdev(values_of_interest)
     print(mean)
     print(st_dev)
-    
+
+plt.show()
