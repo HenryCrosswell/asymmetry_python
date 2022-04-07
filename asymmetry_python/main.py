a824ab054ec672e14b24fc093ad25f0f893c967c
Henry C <henry.junkmail100@gmail.com>
2022-04-07 13:13:04 +0100

    updated main.py with clearer code and clear colours for WT and MT

--- asymmetry_python/main.py
+++ asymmetry_python/main.py
@@ -1,30 +1,46 @@
-from turtle import color
 from utils import read_image, apply_mask, image_mask, mean_and_stdev
 from os import listdir
 import matplotlib.pyplot as plt
 import numpy as np
 folder_path = "C:\\Users\\henry\\OneDrive - University College London\\Project Work\\Image Analysis\\Images\\Cell shaver\\pixel_distance_python\\"
 
-fig, axs = plt.subplots(len(listdir(folder_path)))
+fig, axs = plt.subplots(len(listdir(folder_path)), figsize= (10,20))
 fig.suptitle('Histograms')
-plt.ylabel("quantity")
-plt.ylabel("Pixel distance")
-
+plt.ylabel("Quantity")
+plt.xlabel("Pixel distance")
+plt.rcParams['figure.constrained_layout.use'] = True
 for file_number, file_path in enumerate(listdir(folder_path)):
     input_image = read_image(folder_path+file_path)
     mask = image_mask(input_image)
-    values_of_interest = apply_mask(input_image, mask)
-
-    axs[file_number].hist(values_of_interest)
-    # plt.scatter(np.ones_like(values_of_interest), values_of_interest)
-    #plt.boxplot(values_of_interest)
+    values_of_interest = input_image[mask]
     axs[file_number].set_title(file_path)
-    mean, st_dev = mean_and_stdev(values_of_interest)
-    print(mean)
-    print(st_dev)
     if file_path[:2] == "WT":
-        plt.hist(values_of_interest, color= 'green')
-        print('hi')
-plt.subplots_adjust(left=0.125, bottom=0.1, right=0.9, top=0.9, wspace=0.2, hspace=3)
+        axs[file_number].hist(values_of_interest, color= 'green')
+        continue
+    axs[file_number].hist(values_of_interest)
 
 plt.show()
+
+
+
+#2D array required, need to create a new array with the mask, instead of removing the false values you need to make sure they are not calculated.
+
+
+
+
+
+
+
+#2D array required, need to create a new array with the mask, instead of removing the false values you need to make sure they are not calculated.
+
+
+
+
+
+
+
+
+
+
+
+
