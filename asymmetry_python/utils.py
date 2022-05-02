c58fa32f99528aa1c63368173e91ca45874f4e05
Henry C <henry.junkmail100@gmail.com>
2022-05-02 17:13:38 +0100

    changed utils to include new functions that are used in map script

--- asymmetry_python/utils.py
+++ asymmetry_python/utils.py
@@ -2,6 +2,7 @@ import numpy as np
 from skimage.io import show, imread, imshow
 from pathlib import Path
 import pandas as pd
+from os import listdir
 
 def read_image(file_path):
     input_image = imread(Path(file_path))
@@ -17,13 +18,23 @@ def image_mask(input_image):
             tf_array[i,first_non_zero_index:] = True
     return tf_array
 
-#replaced functionality with boolean indexing - input_image[mask]
-def apply_mask(input_image, mask):
-    df = pd.DataFrame({'booleans': mask.flatten(), 'values': input_image.flatten()}, columns=['booleans', 'values'])
-    masked_image_df = df.loc[df['booleans'] == True, 'values']
-    return masked_image_df.values
-
 def mean_and_stdev(masked_image_values):
     mean = np.mean(masked_image_values)
     st_dev = np.std(masked_image_values)
     return mean, st_dev
+
+def get_pixel_values_from_image_array(xAxis, yAxis, array_of_images):
+    result = []
+    for image in array_of_images:
+        imagePixelValue = image[yAxis][xAxis]
+        result.append(imagePixelValue)
+    return result
+
+def read_and_sort_files(folder_path, wt_list, mt_list):
+    for file_path in listdir(folder_path): 
+        input_image = read_image(folder_path+file_path) 
+
+        if file_path[:2] == "WT":
+            wt_list.append(input_image) 
+        else: 
+            mt_list.append(input_image) 
