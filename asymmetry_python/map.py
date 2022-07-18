722cde1f3e195f655bf76bb33ee857b34e995135
Henry C <henry.junkmail100@gmail.com>
2022-07-18 11:53:27 +0100

    keep record of map and utils

--- asymmetry_python/map.py
+++ asymmetry_python/map.py
@@ -1,12 +1,12 @@
 """Script that sorts images and creates a 3D topographic representation visulising signficant tissue difference"""
 from cmath import nan
-from utils import read_image, get_pixel_values_from_image_array, read_and_sort_files, biggest_value, plot3D
+from utils import read_image, get_pixel_values_from_image_array, read_and_sort_files, biggest_value, plot3Dp_values, plot3Dmedians, var_checked_p_value
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy import stats
 from matplotlib import cm
 from mpl_toolkits.mplot3d import Axes3D
-
+from skimage.transform import rescale
 #real = adjusted_pixel_distance_python
 
 folder_path = "C:\\Users\\henry\\OneDrive - University College London\\Project Work\\Image Analysis\\Images\\Cell shaver\\adjusted_pixel_distance_python\\"
@@ -26,11 +26,13 @@ image_height = len(first_image)
 image_width = len(first_image[0])
 
 #creates a 2D array that is the width and height of the reference image
-result = [[0 for x in range(image_width)] for y in range(image_height)]
+result = [[nan for x in range(image_width)] for y in range(image_height)]
 
-total_result = [[0 for x in range(image_width)] for y in range(image_height)]
-mt_mask = [[nan for x in range(image_width)] for y in range(image_height)]
-wt_mask = [[nan for x in range(image_width)] for y in range(image_height)]
+#total_result = [[0 for x in range(image_width)] for y in range(image_height)]
+p_value_mask = np.array([['#3CAEA300' for x in range(image_width)] for y in range(image_height)], dtype = object)
+
+mt_median_image = [[nan for x in range(image_width)] for y in range(image_height)]
+wt_median_image = [[nan for x in range(image_width)] for y in range(image_height)]
 
 #outside loop is for y axis, inside is for x. Therefore, every time it loops through an x axis the y axis ticks down once
 for current_y_axis in range(image_height):
@@ -40,29 +42,24 @@ for current_y_axis in range(image_height):
         wt_image_pixels = get_pixel_values_from_image_array(current_x_axis, current_y_axis, wt_files)
         mt_image_pixels = get_pixel_values_from_image_array(current_x_axis, current_y_axis, mt_files)
         
-        mean_wt = np.median(wt_image_pixels)
-        mean_mt = np.median(mt_image_pixels)
-
-
+        median_wt = np.median(wt_image_pixels)
+        median_mt = np.median(mt_image_pixels)
         #at the specific pixel value, assesses distributions of both image pixels, if the mean of the WT is greater than the mutant = the P_value is more significant
-        wt_p_value = stats.ttest_ind(wt_image_pixels, mt_image_pixels, alternative='greater').pvalue
+        wt_p_value = var_checked_p_value(wt_image_pixels, mt_image_pixels, 'greater')
+        mt_p_value = var_checked_p_value(wt_image_pixels, mt_image_pixels, 'less')
 
-        mt_p_value = stats.ttest_ind(wt_image_pixels, mt_image_pixels, alternative='less').pvalue
-
-        if mean_wt == 0:
-            mean_wt = nan
-        if mean_mt == 0:
-            mean_mt = nan
-        total_result = mean_wt-mean_mt
-        result[current_y_axis][current_x_axis] = total_result
+        mt_median_image[current_y_axis][current_x_axis] = median_mt
+        wt_median_image[current_y_axis][current_x_axis] = median_wt
+        
+        total_median_values = median_mt-median_wt
+        result[current_y_axis][current_x_axis] = total_median_values
         
         if mt_p_value <= 0.05:
-            mt_mask[current_y_axis][current_x_axis] = mt_p_value
-        if wt_p_value <= 0.05:   
-            wt_mask[current_y_axis][current_x_axis] = wt_p_value
-        #mt_result[currentYAxis][currentXAxis] = mean_mt
-        #largest_pixel_value = biggest_value(largest_pixel_value, result)
+            p_value_mask[current_y_axis][current_x_axis] = '#ED553B'#'#933D86' 
+        if wt_p_value <= 0.05:
+            p_value_mask[current_y_axis][current_x_axis] = '#F6D55C' #'#F178C3'
 
-plot3D(result, mt_mask, wt_mask)
+plot3Dp_values(result, p_value_mask)
+#plot3Dmedians(mt_median_image, wt_median_image)
 
 plt.show()
--- asymmetry_python/utils.py
+++ asymmetry_python/utils.py
@@ -1,40 +1,177 @@
+"""Functions for Asymmetry Project"""
 import numpy as np
 from skimage.io import show, imread, imshow
 from pathlib import Path
-import pandas as pd
 from os import listdir
+from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+from scipy.ndimage import gaussian_filter
+from scipy import stats
+import scipy as sp
+import matplotlib as mpl
+import sys
 
 def read_image(file_path):
+    """Returns an image from the file path."""
     input_image = imread(Path(file_path))
     return input_image
 
 def image_mask(input_image):
-    #returns an array of RGB values from the image in a 2D format
+    """From the input image, returns a True/False array.
+    
+    True -- every value after the first non-zero
+    False -- every zero value up until the first non-zero
+    """
     tf_array = np.zeros(input_image.shape,dtype=bool)
     for i,row in enumerate(input_image):
         first_non_zero_index = (row!=0).argmax()
-        #imagearray - specific row, column to the end of the array
         if first_non_zero_index != -1:
             tf_array[i,first_non_zero_index:] = True
     return tf_array
 
 def mean_and_stdev(masked_image_values):
+    """Returns the mean and standard deviation of a 2D array."""
     mean = np.mean(masked_image_values)
     st_dev = np.std(masked_image_values)
     return mean, st_dev
 
-def get_pixel_values_from_image_array(xAxis, yAxis, array_of_images):
+def get_pixel_values_from_image_array(x_axis, y_axis, array_of_images):
+    """At a specific XY coordinate in all image arrays, return a list of pixel values at the coordinate, removes 0 values.
+    
+    Keyword arguments:
+    x_axis -- the inputed x coordinate
+    y_axis -- the inputed y coordinate
+    array_of_images -- the array of images that will be indexed by the coordinates
+    """
     result = []
     for image in array_of_images:
-        imagePixelValue = image[yAxis][xAxis]
-        result.append(imagePixelValue)
+        imagePixelValue = image[y_axis][x_axis]
+        if imagePixelValue != 0:
+            result.append(imagePixelValue)
     return result
 
 def read_and_sort_files(folder_path, wt_list, mt_list):
+    """Categorises the file into mutant or wild-type, ignoring heterozygous genotypes.
+
+    Keyword arguments:
+    folder_path -- the path from which the images will be obtained
+    wt_list -- an empty list for all wild-type images
+    mt_list -- an empty list for all mutant images
+    """
     for file_path in listdir(folder_path): 
         input_image = read_image(folder_path+file_path) 
-
         if file_path[:2] == "WT":
             wt_list.append(input_image) 
+        elif file_path[:3] == "CF+":
+            continue
         else: 
             mt_list.append(input_image) 
+
+def biggest_value(current_biggest_value, results):
+    """Returns the value if it exceeds the current biggest value."""
+    for value in results:
+        result = np.argmax(value)
+        if np.argmax(value) < current_biggest_value:
+            return current_biggest_value
+        else:
+            return result
+
+
+def plot3Dp_values(result, P_value_mask):
+    """Surface plots the difference in median values onto an X,Y,Z axis, smooths the image and extends the Y axis to a representitive size. 
+    Following this, applys either a Red mask if the P_value for the mutant is significant, or a Blue mask if the wild-type is significant.
+    
+    Keyword arguments:
+    result -- a 2D array, with the diference in median values between indivual pixels for the mutant images and wild-type 
+    mt_mask -- a 2D array, only containing values where the mutant P_value are below 0.05
+    wt_mask -- a 2D array, only containing values where the wild-type P_value are below 0.05
+    """
+    image_height = len(result)
+    image_width = len(result[0])
+    
+    sigma=2.0                  # standard deviation for Gaussian filter
+    truncate=4.0               # truncate filter at this many sigmas
+    
+    U = np.array(result)
+
+    V=U.copy()
+    V[np.isnan(U)]=0
+    VV=sp.ndimage.gaussian_filter(V,sigma=sigma,truncate=truncate)
+
+    W=0*U.copy()+1
+    W[np.isnan(U)]=0
+    WW=sp.ndimage.gaussian_filter(W,sigma=sigma,truncate=truncate)
+
+    Z = VV/WW
+
+    fig, ax = plt.subplots(subplot_kw={"projection": "3d"}, figsize=(8,6))
+    ax.set(xlim=(0, image_width), ylim=(0, image_height))
+    X, Y = np.meshgrid(range(image_width), range(image_height))
+    ax.set_aspect('auto')
+    ax.get_proj = lambda: np.dot(Axes3D.get_proj(ax), np.diag([0.4, 1.0, 0.4, 1]))
+    ax.plot_surface(X,Y,Z, rstride=1, cstride=1, facecolors=P_value_mask, alpha = 0.6)
+    ax.view_init(35, 110)
+    ax.dist = 7
+    median_proxy = mpl.lines.Line2D([0],[0], linestyle="none", c='#3CAEA3', marker = 'o')
+    wt_proxy = mpl.lines.Line2D([0],[0], linestyle="none", c='#F6D55C', marker = 'o')
+    mt_proxy = mpl.lines.Line2D([0],[0], linestyle="none", c='#ED553B', marker = 'o')
+    ax.legend([median_proxy, wt_proxy, mt_proxy], ['Median Difference', 'WT significance', 'MT signficance'], numpoints = 1, loc='upper left')
+
+def plot3Dmedians(result, second_result):
+    """Surface plots both results onto an X,Y,Z axis, smooths the image and extends the Y axis to a representitive size.
+    
+    Keyword arguments:
+    mt_result -- a 2D array, with median values of indivual pixels for the mutant images 
+    wt_result -- a 2D array, with median values of indivual pixels for the wild-type images
+    """
+    image_height = len(result)
+    image_width = len(result[0])
+    filtered_Z_value = {}
+
+    sigma=10                  # standard deviation for Gaussian filter
+    truncate=4.0               # truncate filter at this many sigmas
+
+    function_list = [result, second_result]
+    for i, which_image in enumerate(function_list):
+        U = np.array(which_image)
+
+        V=U.copy()
+        V[np.isnan(U)]=0
+        VV=sp.ndimage.gaussian_filter(V,sigma=sigma,truncate=truncate)
+
+        W=0*U.copy()+1
+        W[np.isnan(U)]=0
+        WW=sp.ndimage.gaussian_filter(W,sigma=sigma,truncate=truncate)
+
+        filtered_Z_value["image {0}".format(i+1)] = VV/WW
+
+    fig, ax = plt.subplots(subplot_kw={"projection": "3d"}, figsize=(8,8))
+    ax.set(xlim=(0, image_width), ylim=(0, image_height))
+    X, Y = np.meshgrid(range(image_width), range(image_height))
+    ax.set_aspect('auto')
+    ax.plot_surface(X, Y, filtered_Z_value["image 1"],  color='g', linewidth=0, antialiased=True, alpha = 0.4, rcount=200, ccount=200)
+    ax.get_proj = lambda: np.dot(Axes3D.get_proj(ax), np.diag([0.4, 1.0, 0.4, 1]))
+
+    ax.plot_surface(X, Y, filtered_Z_value["image 2"], color='r', antialiased=True, alpha = 0.4, rcount=200, ccount=200)
+    ax.get_proj = lambda: np.dot(Axes3D.get_proj(ax), np.diag([0.4, 1.0, 0.4, 1]))
+    ax.dist=5
+
+
+def var_checked_p_value(wt_pixels, mt_pixels, alt_answer):
+    _, unchecked_p_value = stats.levene(wt_pixels, mt_pixels)
+    if unchecked_p_value < 0.05:
+        variance = False
+    else:
+        variance = True
+    p_value = stats.ttest_ind(wt_pixels, mt_pixels, equal_var = variance, alternative=alt_answer).pvalue
+    return p_value
+ 
+def image_dimensions(list_of_files):
+    """ Recieves an image array and returns how many rows and columns it contains."""
+    first_image = list_of_files[0]
+    image_height = len(first_image)
+    image_width = len(first_image[0])
+    return image_width, image_height
+
+
