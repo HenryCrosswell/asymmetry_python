d37a6b6fa0426aeb5cac016f275790398d90250d
Henry C <henry.junkmail100@gmail.com>
2022-08-11 15:27:44 +0100

    changed testing to be more representative and more accurate

--- tests/test_loading.py
+++ tests/test_loading.py
@@ -46,6 +46,5 @@ def test_get_pixel_values_from_image_array():
                 assert len(result) == 5
             if current_x_axis == 1:
                 assert all(result) == 1
-                #assert len(result) == 4
             assert all(result) != 0
 
