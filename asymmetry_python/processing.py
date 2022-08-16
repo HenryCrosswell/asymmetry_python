8adac6ad54e89eebed6a6334478da96e6f4b76b3
Henry Crosswell <52281444+HenryCrosswell@users.noreply.github.com>
2022-08-16 09:10:23 +0100

    added loading bars, formatted for mac... added a new function that quantifies total WT and MT sig, changed if statements to fix median diff bias

--- asymmetry_python/processing.py
+++ asymmetry_python/processing.py
@@ -2,9 +2,14 @@
 Functions that scan the images and run different calculations on them
 """
 from cmath import isnan, nan
-from asymmetry_python.loading import image_dimensions, get_pixel_values_from_image_array
+
+#from asymmetry_python.loading import image_dimensions, get_pixel_values_from_image_array
+from loading import image_dimensions, get_pixel_values_from_image_array
+
 import numpy as np
 from scipy import stats
+from time import sleep
+from tqdm import tqdm
 
 
 def find_and_add_edge(median_diff_array,  p_value_mask, line_width, colour, value):
@@ -25,10 +30,11 @@ def find_and_add_edge(median_diff_array,  p_value_mask, line_width, colour, valu
             first_value_index = nan_indices[0][-1] + 1
             indexed_line_width = first_value_index + line_width
             p_value_mask[y_axis,first_value_index:indexed_line_width] = colour
-            median_diff_array[y_axis,indexed_line_width-4:indexed_line_width+4] = nan
+            median_diff_array[y_axis,indexed_line_width-7:indexed_line_width+7] = nan
             median_diff_array[y_axis,first_value_index:indexed_line_width] = value
         else:
-            p_value_mask[y_axis,0:5] = colour
+            p_value_mask[y_axis,0:line_width] = colour
+            median_diff_array[y_axis,0:line_width] = value
             
     return p_value_mask, median_diff_array
 
@@ -54,18 +60,37 @@ def var_checked_p_value(wt_pixels, mt_pixels):
     mt_pixels -- A list of pixel values at a specific coordinate from the MT images.
     alt_answer -- Determined by a pilot study, defines the alternative hypothesis.
     """
-    if np.mean(wt_pixels) >= np.mean(mt_pixels):
+    wt_mean = np.mean(wt_pixels)
+    mt_mean = np.mean(mt_pixels)
+    
+    if wt_mean >= mt_mean:
         name_of_higher_mean_embryos = 'wt_mean'
     else:
         name_of_higher_mean_embryos = 'mt_mean'
     
+    #error code here at y230 -  x473
+   
     _, unchecked_p_value = stats.levene(wt_pixels, mt_pixels)
+
     if unchecked_p_value < 0.05:
         variance = False
     else:
         variance = True
     p_value = stats.ttest_ind(wt_pixels, mt_pixels, equal_var = variance).pvalue
     return p_value, name_of_higher_mean_embryos
+
+def total_significant_values(p_value_mask, median_diff_array):
+    ''' calculates percentage of WT and MT significance as a percentage of area.'''
+
+    wt_sig_values = len(np.where(p_value_mask == '#F6D55C'))
+    mt_sig_values = len(np.where(p_value_mask == '#ED553B'))
+    total_non_nans = np.count_nonzero(~np.isnan(median_diff_array))
+
+    wt_sig_percentage = (wt_sig_values/total_non_nans)*100
+    mt_sig_percentage = (mt_sig_values/total_non_nans)*100
+
+    return round(wt_sig_percentage, 0), round(mt_sig_percentage, 0)
+
 
 def scan_image_and_process(wt_files, mt_files):
     """ From the list of WT and MT files, scans through each image pixel and assigns the values to a seperate list, at a certain x and y coordinate.
@@ -83,10 +108,12 @@ def scan_image_and_process(wt_files, mt_files):
     wt_median_image = [[nan for x in range(image_width)] for y in range(image_height)]
     median_diff_array = [[nan for x in range(image_width)] for y in range(image_height)]
     p_value_mask_array = np.array([['None' for x in range(image_width)] for y in range(image_height)], dtype = object)
-    
+    print(' Processing files...')
+    pbar = tqdm(total = (image_height))
     for current_y_axis in range(image_height):
+        sleep(0.02) 
+        pbar.update(1)
         for current_x_axis in range(image_width):
-
             #returns a list of values at the current x and y coordinate for either the wt or mt images. 
             wt_image_pixels = get_pixel_values_from_image_array(current_x_axis, current_y_axis, wt_files)
             mt_image_pixels = get_pixel_values_from_image_array(current_x_axis, current_y_axis, mt_files)
@@ -94,28 +121,29 @@ def scan_image_and_process(wt_files, mt_files):
             wt_image_pixels = threshold(wt_image_pixels)
             mt_image_pixels = threshold(mt_image_pixels)
 
-            if len(wt_image_pixels) >=2 and len(mt_image_pixels) >=2:
-
+            if len(wt_image_pixels) >=2:
                 median_wt = np.median(wt_image_pixels)
-                median_mt = np.median(mt_image_pixels)
-
                 wt_median_image[current_y_axis][current_x_axis] = median_wt
+            else:
+                wt_median_image[current_y_axis][current_x_axis] = nan
+
+            if len(mt_image_pixels) >=2:
+                median_mt = np.median(mt_image_pixels)
                 mt_median_image[current_y_axis][current_x_axis] = median_mt
+            else:
+                mt_median_image[current_y_axis][current_x_axis] = nan
+
+            if len(mt_image_pixels) >=2 and len(wt_image_pixels) >=2:
 
                 median_diff_array[current_y_axis][current_x_axis] = median_mt-median_wt
-
                 p_value, name_of_higher_mean_embryos = var_checked_p_value(wt_image_pixels, mt_image_pixels)
                 if p_value <= 0.05:
                     if name_of_higher_mean_embryos == 'wt_mean':
                         p_value_mask_array[current_y_axis][current_x_axis] = '#F6D55C' 
                     else:
                         p_value_mask_array[current_y_axis][current_x_axis] = '#ED553B'
-                            
-            # empty lists are removed to avoid runtime errors.
             else:
-                mt_median_image[current_y_axis][current_x_axis] = nan
-                wt_median_image[current_y_axis][current_x_axis] = nan
                 median_diff_array[current_y_axis][current_x_axis] = nan
-
+    
     return median_diff_array, p_value_mask_array, mt_median_image, wt_median_image
 
