32e65105822167c409ff98425e74f1b4ea72c5d7
Henry Crosswell <henrycrosswell@PD1071.local>
2023-08-26 18:37:21 +0100

    removed redundant test

--- asymmetry_python/tests/conftest.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-
-# Get the root directory of your project (where main.py is located)
-root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-
-# Add the root directory to the system path
-sys.path.insert(0, root_dir)
